0c793fe39d9c91bccc1245ae11578dce90507773
Robert Cook <robert.cook@mindgard.ai>
2025-09-16 12:18:24 +0100

    Added guardrail bypass datasets to attack configuration options
    Co-authored-by: Imran Bohoran <imran.bohoran@mindgard.ai>

--- src/main/java/ai/mindgard/sandbox/Dataset.java
+++ src/main/java/ai/mindgard/sandbox/Dataset.java
@@ -9,19 +9,27 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 public enum Dataset {
-    Default(null),
-    CustomerService("BadCustomer"),
-    Finance("BadFinance"),
-    Legal("BadLegal"),
-    Medical("BadMedical"),
-    Injection("SqlInjection"),
-    XSS("Xss");
+    Default(null, "Default"),
+    CustomerService("BadCustomer", "CustomerService"),
+    Finance("BadFinance", "Finance"),
+    Legal("BadLegal", "Legal"),
+    Medical("BadMedical", "Medical"),
+    Injection("SqlInjection", "Injection"),
+    XSS("Xss", "XSS"),
+    GuardrailBypassDeepset("GuardrailBypassDeepset", "Bypass - Deepset (experimental)"),
+    GuardrailBypassLlmGuard("GuardrailBypassLlmGuard", "Bypass - LlmGuard (experimental)"),
+    GuardrailBypassMetaPromptGuardV1("GuardrailBypassMetaPromptGuardV1", "Bypass - MetaPromptGuardV1 (experimental)"),
+    GuardrailBypassProtectAiV1("GuardrailBypassProtectAiV1", "Bypass - ProtectAiV1 (experimental)"),
+    GuardrailBypassProtectAiV2("GuardrailBypassProtectAiV2", "Bypass - ProtectAiV2 (experimental)"),
+    GuardrailBypassVijilPromptInjection("GuardrailBypassVijilPromptInjection", "Bypass - VijilPromptInjection (experimental)");
 
 
     private final String datasetName;
+    private final String displayName;
 
-    Dataset(String datasetName) {
+    Dataset(String datasetName, String displayName) {
         this.datasetName = datasetName;
+        this.displayName = displayName;
     }
 
     public static int indexOfName(String dataset) {
@@ -47,6 +55,15 @@ public enum Dataset {
 
     public String getDatasetName() {
         return datasetName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
     }
 
 }
