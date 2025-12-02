28fff327e7b0f3d612a189ce0879b8afd5ed136a
ASACookCodes <andrew.cook@mindgard.ai>
2025-12-02 11:44:30 +0000

    Constants from MindgardAuthentication moved to Constants.
    Co-authored-by: Rob Heath <robert.heath@mindgard.ai>

--- src/main/java/ai/mindgard/Constants.java
+++ src/main/java/ai/mindgard/Constants.java
@@ -4,6 +4,9 @@ public final class Constants {
     public static final String ORCHESTRATOR_API_URL = "https://api.sandbox.mindgard.ai";
     public static final String FRONTEND_URL = "https://sandbox.mindgard.ai";
     public static final String SETTINGS_FILE_NAME = "burp.json";
+    public static final String CLIENT_ID = "U0OT7yZLJ4GEyabar11BENeQduu4MaNO";
+    public static final String AUDIENCE = "https://marketplace-orchestrator.com";
+    public static final String LOGIN_DOMAIN = "https://login.sandbox.mindgard.ai";
 
     private Constants() {}
 }
