bb4c90ea07692fd60af19e2f3175c65ef1fa19af
Andrew Cook <andrew.cook@mindgard.ai>
2026-02-16 13:36:14 +0000

    Changed constants for tenancy * Sandbox being decommissioned.

--- src/main/java/ai/mindgard/Constants.java
+++ src/main/java/ai/mindgard/Constants.java
@@ -1,13 +1,13 @@
 package ai.mindgard;
 
 public final class Constants {
-    public static final String ORCHESTRATOR_API_URL = "https://api.sandbox.mindgard.ai";
-    public static final String FRONTEND_URL = "https://sandbox.mindgard.ai";
+    public static final String ORCHESTRATOR_API_URL = "https://api.demo.us.mindgard.ai";
+    public static final String FRONTEND_URL = "https://demo.us.mindgard.ai";
     public static final String SETTINGS_FILE_NAME = "burp.json";
     public static final String TOKEN_FILE_NAME = "token.json";
-    public static final String CLIENT_ID = "U0OT7yZLJ4GEyabar11BENeQduu4MaNO";
-    public static final String AUDIENCE = "https://marketplace-orchestrator.com";
-    public static final String LOGIN_DOMAIN = "https://login.sandbox.mindgard.ai";
+    public static final String CLIENT_ID = "mBSDAk4R283BeGC17Uvbyo3vSAAGL67J";
+    public static final String AUDIENCE = "https://prod-pov-demo-orchestrator.com/";
+    public static final String LOGIN_DOMAIN = "https://login.demo.us.mindgard.ai";
 
     private Constants() {}
 }
